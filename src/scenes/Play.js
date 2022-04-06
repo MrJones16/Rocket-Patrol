087f33afacb97308e68696d0bb40e1dc42be3552
Peyton <projectile212@gmail.com>
2022-04-06 15:26:38 -0700

    Added borders to play scene and files for the game

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,9 +1,16 @@
-class Player extends Phaser.Scene {
+class Play extends Phaser.Scene {
     constructor(){
-        super("PlayerScene");
+        super("playScene");
     }
 
     create(){
-        this.add.text(20,20, "Rocket Patrol Play");
+        //Green UI Background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00ff00).setOrigin(0,0);
+        //White borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        
     }
 }
